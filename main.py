f1e67bab25f2d65965da31ebf26c03c47d582dfb
Anvith C L <anvithchakote33@gmail.com>
2026-06-14 14:50:39 +0530

    fully completed weather app

--- main.py
+++ main.py
@@ -6,19 +6,22 @@ city = input("Enter city name: ")
 
 url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
 
-response = requests.get(url)
+try:
+    response = requests.get(url)
 
-data = response.json()
-if data["cod"] == 200:
-    main = data["main"]
-    temperature = main["temp"] - 273.15  # Convert from Kelvin to Celsius
-    humidity = main["humidity"]
-    weather_description = data["weather"][0]["description"]
+    data = response.json()
+    if data["cod"] == 200:
+        main = data["main"]
+        temperature = main["temp"] - 273.15  # Convert from Kelvin to Celsius
+        humidity = main["humidity"]
+        weather_description = data["weather"][0]["description"]
 
-    print(f"\n***Weather Report***")
-    print(f"City: {city}")
-    print(f"Temperature: {temperature:.2f}°C")
-    print(f"Humidity: {humidity}%")
-    print(f"Weather Description: {weather_description}")
-else:
-    print("City not found.")
+        print(f"\n----Weather Report----")
+        print(f"City: {city}")
+        print(f"Temperature: {temperature:.2f}°C")
+        print(f"Humidity: {humidity}%")
+        print(f"Weather Description: {weather_description}")
+    else:
+        print("City not found.")
+except Exception:
+    print("Unable to fetch weather data.")
